9879353e89f8e943e06971662d58d6903807ef48
ousama lasri <oussama.lasri.2k@gmail.com>
2026-01-27 14:01:35 +0100

    feat : add service layer

---
+++ src/main/java/com/oussma/foodcatalogue/service/FoodCatalogueService.java
@@ -0,0 +1,53 @@
+package com.oussma.foodcatalogue.service;
+
+import com.oussma.foodcatalogue.dto.FoodCataloguePage;
+import com.oussma.foodcatalogue.dto.FoodItemDTO;
+import com.oussma.foodcatalogue.dto.Restaurant;
+import com.oussma.foodcatalogue.entity.FoodItem;
+import com.oussma.foodcatalogue.mapper.FoodItemMapper;
+import com.oussma.foodcatalogue.repository.FoodItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class FoodCatalogueService {
+
+    @Autowired
+    FoodItemRepository foodItemRepo;
+
+    @Autowired
+    RestTemplate restTemplate;
+    
+    @Autowired
+    FoodItemMapper foodItemMapper;
+
+
+    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+        FoodItem foodItemSavedInDB = foodItemRepo.save(foodItemMapper.mapFoodItemDTOToFoodItem(foodItemDTO));
+        return foodItemMapper.mapFoodItemToFoodItemDto(foodItemSavedInDB);
+    }
+
+    public FoodCataloguePage fetchFoodCataloguePageDetails(Integer restaurantId) {
+        List<FoodItem> foodItemList =  fetchFoodItemList(restaurantId);
+        Restaurant restaurant = fetchRestaurantDetailsFromRestaurantMS(restaurantId);
+        return createFoodCataloguePage(foodItemList, restaurant);
+    }
+
+    private FoodCataloguePage createFoodCataloguePage(List<FoodItem> foodItemList, Restaurant restaurant) {
+        FoodCataloguePage foodCataloguePage = new FoodCataloguePage();
+        foodCataloguePage.setFoodItemsList(foodItemList);
+        foodCataloguePage.setRestaurant(restaurant);
+        return foodCataloguePage;
+    }
+
+    private Restaurant fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
+       return restTemplate.getForObject("http://RESTAURANT-SERVICE/restaurant/fetchById/"+restaurantId, Restaurant.class);
+    }
+
+    private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
+        return foodItemRepo.findByRestaurantId(restaurantId);
+    }
+}
