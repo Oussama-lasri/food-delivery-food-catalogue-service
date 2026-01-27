201af70a52db83975ddc71370675826925787510
ousama lasri <oussama.lasri.2k@gmail.com>
2026-01-27 13:47:16 +0100

    feat : add repository

---
+++ src/main/java/com/oussma/foodcatalogue/repository/FoodItemRepository.java
@@ -0,0 +1,13 @@
+package com.oussma.foodcatalogue.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oussma.foodcatalogue.entity.FoodItem;
+
+@Repository
+public interface FoodItemRepository  extends JpaRepository<FoodItem, Integer> {
+    List<FoodItem> findByRestaurantId(Integer restaurantId);
+}
