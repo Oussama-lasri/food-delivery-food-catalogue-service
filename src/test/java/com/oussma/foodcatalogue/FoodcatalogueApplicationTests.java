5e0de84feb4f98950581f8e83d1acbd8073d5cc9
ousama lasri <oussama.lasri.2k@gmail.com>
2026-04-22 19:20:06 +0100

    test : fix issue of testing

--- src/test/java/com/oussma/foodcatalogue/FoodcatalogueApplicationTests.java
+++ src/test/java/com/oussma/foodcatalogue/FoodcatalogueApplicationTests.java
@@ -2,9 +2,17 @@ package com.oussma.foodcatalogue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.oussma.foodcatalogue.mapper.FoodItemMapper;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FoodcatalogueApplicationTests {
+	
+	  @MockitoBean
+	    private FoodItemMapper foodItemMapper;
 
 	@Test
 	void contextLoads() {
