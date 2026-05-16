dbafb786f10beea5d615b5a66207a960e4e83e78
ousama lasri <oussama.lasri.2k@gmail.com>
2026-05-16 17:04:35 +0100

    test

--- src/main/java/com/oussma/foodcatalogue/controller/FoodCatalogueController.java
+++ src/main/java/com/oussma/foodcatalogue/controller/FoodCatalogueController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class FoodCatalogueController {
 
+	
 
     private final FoodCatalogueService foodCatalogueService;
 
